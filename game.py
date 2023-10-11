53d89eed6f11db28670f39d3bbb204c2b4fcb60e
Zach Klagge <klagge.zach@gmail.com>
2023-10-11 15:14:05 -0500

    Basic searchlight

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 
 # Initialize pygame
 pygame.init()
@@ -11,7 +12,11 @@ BACKGROUND_COLOR = (0, 0, 0)
 # Dreadnought configuration
 DREADNOUGHT_COLOR = (255, 255, 255)
 DREADNOUGHT_SIZE = 50
-SPEED = 0.01  # Adjust the speed of the dreadnought's movement
+SPEED = 0.01
+
+# Searchlight configuration
+SEARCHLIGHT_COLOR = (255, 255, 0)
+SEARCHLIGHT_RADIUS = 100
 
 # Main game loop
 def game_loop():
@@ -22,6 +27,7 @@ def game_loop():
 
     while True:
         screen.fill(BACKGROUND_COLOR)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
         # Event handling
         for event in pygame.event.get():
@@ -42,8 +48,18 @@ def game_loop():
         # Drawing the dreadnought
         pygame.draw.rect(screen, DREADNOUGHT_COLOR, (*dreadnought_position, DREADNOUGHT_SIZE, DREADNOUGHT_SIZE))
 
+        # Drawing the searchlight
+        dx = mouse_x - dreadnought_position[0]
+        dy = mouse_y - dreadnought_position[1]
+        angle = math.atan2(dy, dx)
+        end_x = dreadnought_position[0] + math.cos(angle) * SEARCHLIGHT_RADIUS
+        end_y = dreadnought_position[1] + math.sin(angle) * SEARCHLIGHT_RADIUS
+        pygame.draw.line(screen, SEARCHLIGHT_COLOR, (dreadnought_position[0] + DREADNOUGHT_SIZE // 2, 
+                                                    dreadnought_position[1] + DREADNOUGHT_SIZE // 2), (end_x, end_y), 3)
+
         # Update the display
         pygame.display.flip()
 
 # Run the game loop
 game_loop()
+
